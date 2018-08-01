d3a1a07d6085eb216f5ea7afb38f00f36c2c4b41
AishwaryaVeer <41576536+AishwaryaVeer@users.noreply.github.com>
2018-08-01 01:11:58 -0700

    Add files via upload

---
+++ Assignment8_WCF_Part2/SelfHosting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using CardInfoLibrary;
+
+namespace SelfHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Validate));
+            host.Open();
+            Console.WriteLine("Server Started....");
+            Console.WriteLine("Press any key to close server");
+            Console.ReadLine();
+        }
+    }
+}
